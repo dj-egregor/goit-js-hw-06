dc230b4e97e5cefb00ad41d6523f6319e07c6089
Alexander Kushnir <dj.egregor1@gmail.com>
2022-09-23 10:41:01 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const list = document.querySelector('ul#ingredients');
+
+function createItem(text) {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = text;
+  return itemEl;
+}
+
+const items = ingredients.map(createItem);
+list.append(...items);
