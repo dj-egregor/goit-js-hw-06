a84f1e8b257055c4c1b435285c1232ccd9e7e1bd
Alexander Kushnir <dj.egregor1@gmail.com>
2022-09-23 20:53:24 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEL = document.querySelector('body');
+const colorValueEl = document.querySelector('span.color');
+const btnChangeColor = document.querySelector('button.change-color');
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyEL.style.backgroundColor = color;
+  colorValueEl.textContent = color;
+}
+
+btnChangeColor.addEventListener('click', changeColor);
