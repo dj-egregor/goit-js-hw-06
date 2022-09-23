09ecdcf0015a4652918deed39d0bc42dee383113
Alexander Kushnir <dj.egregor1@gmail.com>
2022-09-23 19:59:16 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('form.login-form');
 
+function checkFormData(event) {
+  const email = formEl.elements.email.value;
+  const password = formEl.elements.password.value;
+  if (email === '' || password === '') {
+    alert('Все поля формы должны быть заполнеы!');
+  } else {
+    const sendData = {
+      [formEl.elements.email.name]: email,
+      [formEl.elements.password.name]: password,
+    };
+    console.log(sendData);
+    formEl.reset();
+  }
+  event.preventDefault();
+}
+
+formEl.addEventListener('submit', checkFormData);
